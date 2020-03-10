c845d267b7a5006f6f47e3599c9090f63ae96757
keyhad <keyvan.ha@gmail.com>
2020-03-10 22:13:48 +0100

    more values

--- TouchDetection/test.c
+++ TouchDetection/test.c
@@ -3,7 +3,7 @@
 #include "TouchDetection.h"
 
 // help data & functions
-uint8_t test_forces[] = {1, 2, 0, 0, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 0, 0, 0, 0};
+uint8_t test_forces[] = {1, 2, 0, 0, 3, 4, 5, 6, 7, 8, 9, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 0, 1, 2, 3, 4, 0, 0, 0, 0};
 
 uint8_t getForce(void* pHandle, int col, int row)
 {
@@ -17,7 +17,8 @@ void test_detectTouches()
 {
     detectTouches(test_forces);
     
-    printf("#1 %s\n", _positions[0]._force ? "PASS" : "FAIL");
+    printf("#1-1 %s\n", _positions[0]._force ? "PASS" : "FAIL");
+    printf("#1-2 %s\n", _positions[1]._force ? "PASS" : "FAIL");
 }
 
 void main() 
